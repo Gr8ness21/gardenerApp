cd7a623d184e62a1f57650a819da505a123fe2d4
Gr8ness21 <pmleechapman@gmail.com>
2026-07-08 17:01:02 -0400

    Created initial logic for gardener app

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// This file contains the logic behind our gardener/landscaper app!
+// Establish Variables
+let player;
+let lawn;
+let cut;
+let tools; // Hands, Scissors, Push Mowers, Riding Mowers, Army of goats, Army of goats with lawnmowers
+let money = 5;
+
+if(money >= 100){
+    console.log("You got scissors!")
+} else {
+    console.log("Continue using your hands until you get your money up!")
+}
